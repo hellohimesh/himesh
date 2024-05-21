e1acd692c8d5c0f1e0720b0ec2d51ff1d67ef1bf
hellohimesh <himeshssuthar@gmail.com>
2024-05-21 09:53:29 +0530

    build

---
+++ precache-manifest.a85fa723cf042edf56dd2d592dc792c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be6ba296632b077fe8d90f6fdaa093a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9975febae6f1d40a65",
+    "url": "/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "fa39e0e779cded4ec142",
+    "url": "/static/css/main.ccac253c.chunk.css"
+  },
+  {
+    "revision": "2e9975febae6f1d40a65",
+    "url": "/static/js/2.f2d9d01d.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.f2d9d01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa39e0e779cded4ec142",
+    "url": "/static/js/main.ae207cd0.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
